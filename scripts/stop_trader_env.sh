8d942bffb55808a263dbf7fec44ed0c06656e6f3
Vraj Pandya <vrajspandya@gmail.com>
2023-10-10 00:25:04 -0700

    add stop trader script

---
+++ scripts/stop_trader_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Note: Try running the script from the project root directory
+echo "if there are errors, try running the script from the project root directory"
+
+# Stop the mongo server
+# kill mongod jack hammer
+killall -9 mongod
